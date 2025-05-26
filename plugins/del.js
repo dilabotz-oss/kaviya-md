9ae185188eb0e66ee7d2d328fd7f53a45a05e0b0
kaviya353 <kaviyamd8@gmail.com>
2025-05-26 16:45:23 +0100

    Update del.js

--- plugins/del.js
+++ plugins/del.js
@@ -23,6 +23,6 @@ const key = {
         await conn.sendMessage(m.chat, { delete: key })
 } catch(e) {
 console.log(e);
-reply('🔥𝐁.𝐌.𝐁-𝐗𝐌𝐃🔥 successful..👨‍💻✅')
+reply('🔥KAVIYA MD🔥 successful..👨‍💻✅')
 } 
 })
