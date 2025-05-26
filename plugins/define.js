3f187645b64c2e2167acf6efff48653e38175536
kaviya353 <kaviyamd8@gmail.com>
2025-05-26 16:44:57 +0100

    Update define.js

--- plugins/define.js
+++ plugins/define.js
@@ -28,13 +28,13 @@ const wordInfo = `
 📝 *Example*: ${example}
 🔗 *Synonyms*: ${synonyms}
 
-> *@ 💫 𝐁.𝐌.𝐁-𝐗𝐌𝐃 💫*`;
+> *@ 💫 KAVIYA MD 💫*`;
 
                                  return reply(wordInfo);
                              } catch (e) {
                                  console.log(e);
                                  if (e.response && e.response.status === 404) {
-                                     return reply("🚫💫𝐁.𝐌.𝐁-𝐗𝐌𝐃💫 Word not found. Please check the spelling and try again.");
+                                     return reply("🚫💫KAVIYA MD💫 Word not found. Please check the spelling and try again.");
                                  }
                                  return reply("⚠️ An error occurred while fetching the definition. Please try again later.");
                              }
