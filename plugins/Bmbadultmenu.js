f253a92a629b7acfd6f46dc218271e186feea7a5
kaviya353 <kaviyamd8@gmail.com>
2025-05-27 16:39:58 +0100

    Update Bmbadultmenu.js

--- plugins/Bmbadultmenu.js
+++ plugins/Bmbadultmenu.js
@@ -23,7 +23,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         await conn.sendMessage(
             from,
             {
-                image: { url: `https://files.catbox.moe/lph7xd.jpg` },
+                image: { url: `https://i.ibb.co/201XgZ5f/SulaMd.jpg` },
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
@@ -31,7 +31,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
                         newsletterJid: '120363382023564830@newsletter',
-                        newsletterName: "𝐁.𝐌.𝐁-𝐗𝐌𝐃  𝐀𝐃𝐔𝐋𝐓 𝐌𝐄𝐍𝐔🔞🔞₊",
+                        newsletterName: "KAVIYA MD  𝐀𝐃𝐔𝐋𝐓 𝐌𝐄𝐍𝐔🔞🔞₊",
                         serverMessageId: 143
                     }
                 }
