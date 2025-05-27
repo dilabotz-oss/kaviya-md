1e683c012be5894b4b4e90f2525847ef611c0352
kaviya353 <kaviyamd8@gmail.com>
2025-05-27 17:01:31 +0100

    Update prayertime.js

--- plugins/prayertime.js
+++ plugins/prayertime.js
@@ -53,7 +53,7 @@ async(conn, mek, m, {from, l, quoted, body, isCmd, command, args, q, isGroup, se
         await conn.sendMessage(
             from,
             {
-                image: { url: `https://files.catbox.moe/s7pzei.jpg` }, // Image URL here
+                image: { url: `https://i.ibb.co/201XgZ5f/SulaMd.jpg` }, // Image URL here
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
@@ -61,7 +61,7 @@ async(conn, mek, m, {from, l, quoted, body, isCmd, command, args, q, isGroup, se
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
                         newsletterJid: '120363382023564830@newsletter',
-                        newsletterName: '🔥𝐁.𝐌.𝐁-𝐗𝐌𝐃🔥',
+                        newsletterName: '🔥KAVIYA MD🔥',
                         serverMessageId: 143
                     }
                 }
