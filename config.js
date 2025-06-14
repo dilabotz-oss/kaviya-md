51d8f948fe88f180d4ec5d8a3e29c54b66d21f91
dilabotz-oss <ddws650@gmail.com>
2025-06-14 12:08:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "12Mx2CyK#WaMMH2m38SuoJdS0JOFhUwRwVZ4r-tG0JrRElVyIPEA",
+SESSION_ID: process.env.SESSION_ID || "L0p1mTLB#7KiGCRTRkponFwfyY6EQLfR8sQBtCWz3GiZLGh_qKDs",
 // add your Session Id 
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot
@@ -17,7 +17,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "+94776078258",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "+94760224138",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "KAVIYA MD",
 // add bot owner name
@@ -53,7 +53,7 @@ HEART_REACT: process.env.HEART_REACT || "false",
 // make this true or false for heart reactions only 
 OWNER_REACT: process.env.OWNER_REACT || "true",
 // make it true or fasle for only react on owner msg only 
-ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
 // maks true for always online 
 PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
 // make false if want private mod
